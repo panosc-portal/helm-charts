c23b1e91397356a252bd67a08b2685685e38a3e8
William <turnerw@ill.fr>
2020-03-19 13:00:02 +0100

    Added desktopService database to panoscPostgres and packaged it to v0.1.2

--- panosc-postgres/postgres-config-files/panosc-portal-database.sh
+++ panosc-postgres/postgres-config-files/panosc-portal-database.sh
@@ -9,9 +9,12 @@ echo "Creating users and databases"
 $POSTGRES <<EOSQL
 CREATE USER "cloud-provider-kubernetes" WITH CREATEDB PASSWORD '${CLOUD_PROVIDER_K8S_DATABASE_PASSWORD}';
 CREATE USER "cloud-service" WITH CREATEDB PASSWORD '${CLOUD_SERVICE_DATABASE_PASSWORD}';
+CREATE USER "desktop-service" WITH CREATEDB PASSWORD '${DESKTOP_SERVICE_DATABASE_PASSWORD}';
 CREATE DATABASE "cloud-provider-kubernetes" OWNER "cloud-provider-kubernetes";
 CREATE DATABASE "cloud-service" OWNER "cloud-service";
+CREATE DATABASE "desktop-service" OWNER "desktop-service";
 EOSQL
 
 $POSTGRES -d cloud-provider-kubernetes -a -f /tmp/cloud-provider-kubernetes.sql;
 $POSTGRES -d cloud-service -a -f /tmp/cloud-service.sql;
+$POSTGRES -d desktop-service -a -f /tmp/desktop-service.sql;
